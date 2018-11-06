706e0ec1efbed9e87223a4c93e2ba698a4f30aaa
Joni Collinge <jonathancollinge@live.com>
2018-11-06 08:00:06 +0000

    Update variables.tf
    Added more fake variables to test rendering

--- variables.tf
+++ variables.tf
@@ -1,3 +1,27 @@
+variable "azure_directory" {
+  description = "Your Azure directory."
+}
+
+variable "azure_subscription" {
+  description = "Your Azure subscription."
+}
+
+variable "resource_group" {
+  description = "Resource group name to deploy."
+}
+
+variable "site_location" {
+  description = "The location for the site deployment."
+}
+
+variable "site_name" {
+  description = "The name of the site to deploy."
+}
+
+variable "sku" {
+  description = "The sku of the site."
+}
+
 variable "dnsimple_domain" {
   description = "The domain we are creating a record for."
 }
